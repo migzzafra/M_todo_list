8e3891886046c492c963f7c32ec2c86a997f03fc
migzzafra <94744309+migzzafra@users.noreply.github.com>
2026-09-13 22:51:20 +0800

    added initializeApp function
    This will enable the app to load the saved projects, if there any. Otherwise, it will create a default one.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
+import { initializeDefaultProject, loadProjects } from "./projectlist.js";
+import { loadState, saveState } from "./storagemananement.js";
 import "./styles.css";
+
+function initializeApp() {
+    const result = loadState();
+
+    if (result === null) {
+        initializeDefaultProject();
+    } else {
+        loadProjects(result);
+        
+    }
+    
+    saveState();
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    initializeApp();
+})
