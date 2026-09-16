5f6683d0e1885c960e1126d3403e673350065f0a
migzzafra <94744309+migzzafra@users.noreply.github.com>
2026-09-16 23:44:17 +0800

    Update webpack.common.js

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,6 @@
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
  const __filename = fileURLToPath(import.meta.url);
  const __dirname = path.dirname(__filename);
@@ -8,22 +9,17 @@ import { fileURLToPath } from 'node:url';
    entry: {
      app: './src/index.js',
    },
-   experiments: {
-     html: true,
-   },
    output: {
      filename: '[name].bundle.js',
      htmlFilename: 'index.html',
      path: path.resolve(__dirname, 'dist'),
      clean: true,
-     html: {
-       meta: {
-         charset: 'UTF-8',
-         viewport: 'width=device-width, initial-scale=1',
-       },
-       title: 'Production',
-     },
    },
+   plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/app.html",
+    }),
+  ],
    module: {
     rules:[
       {
